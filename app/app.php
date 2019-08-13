ccb414b301b0a897a313cbc8134499d49794526d
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2019-08-14 00:59:12 +0200

    App script and connect file for DB credentials

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="Content-Language" content="en">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>SZONs</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+        <meta name="description" content="Szons">
+        <meta name="msapplication-tap-highlight" content="no">
+        <link href="./assets/styles/main.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">Wczytywanie...</div>
+        <script src="bundle.js"></script>
+    </body>
+</html>
