6bf478106e44c584d0a91bb65dda3ae196fde6c7
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2019-09-14 21:02:53 +0200

    Tenants list moved to react component

--- src/PageTenants.js
+++ src/PageTenants.js
@@ -5,54 +5,21 @@ import 'bootstrap';
 import Utils from './Utils';
 import MTable from './MTable';
 import PageHeader from './PageHeader';
-import LoaderComponent from './LoaderComponent';
+import TenantsComponent from './TenantsComponent';
 
 class PageTenants extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            data: false
-        };
-    }
-    createTableStructure(data, apartments) {
-        return {
-            title: false,
-            clickableHash: "tenants",
-            headers: ["Id", "Imię", "Nazwisko", "Mieszkanie", "Pokój", "Saldo"],
-            rows: data.map(x => {
-                x.apartment_id = Utils.findArrValById(
-                    apartments.map(a => [a.id, a.name]),
-                    x.apartment_id
-                );
-                return Object.values(x);
-            })
-        };
-    }
-    componentDidMount() {
-        $.when(
-            $.get("/api.php?r=tenants"),
-            $.get("/api.php?r=apartments")
-        ).then((tenantData, apartmentsData) => {
-            this.setState(() => {
-                return {
-                    data: this.createTableStructure(tenantData[0], apartmentsData[0]),
-                }
-            });
-        });
     }
     render() {
         return (
             <div className="app-main__inner">
-                {this.state.data ? <React.Fragment>
                     <PageHeader {...this.props.headerData} />
                     <div className="row">
                         <div className="col-md-12">
-                            <div className="main-card mb-3 card">
-                                <MTable tableData={this.state.data} />
-                            </div>
+                            <TenantsComponent />
                         </div>
                     </div>
-                </React.Fragment> : <LoaderComponent />}
             </div>
         )
     }
---
+++ src/TenantsComponent.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Utils from './Utils';
+import MTable from './MTable';
+import LoaderComponent from './LoaderComponent';
+
+class TenantsComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: false
+        };
+    }
+    createTableStructure(data, apartments) {
+        return {
+            title: false,
+            clickableHash: "tenants",
+            headers: ["Id", "Imię", "Nazwisko", "Mieszkanie", "Pokój", "Czynsz"],
+            rows: data.map(x => {
+                x.apartment_id = Utils.findArrValById(
+                    apartments.map(a => [a.id, a.name]),
+                    x.apartment_id
+                );
+                return Object.values(x);
+            })
+        };
+    }
+    componentDidMount() {
+        $.when(
+            $.get("/api.php?r=tenants"),
+            $.get("/api.php?r=apartments")
+        ).then((tenantData, apartmentsData) => {
+            this.setState(() => {
+                return {
+                    data: this.createTableStructure(tenantData[0], apartmentsData[0]),
+                }
+            });
+        });
+    }
+    render() {
+        return (
+            <div className="main-card mb-3 card">
+                {this.state.data ? <React.Fragment>
+                    <MTable tableData={this.state.data} />
+                </React.Fragment> : <LoaderComponent />}
+            </div>
+        )
+    }
+}
+
+export default TenantsComponent;
