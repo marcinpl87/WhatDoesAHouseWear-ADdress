1b07554aa1fd2ac48b2cfc1a3839354a66db17eb
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2019-09-18 23:49:09 +0200

    Chart component extended

--- src/ChartComponent.js
+++ src/ChartComponent.js
@@ -6,11 +6,36 @@ class ChartComponent extends React.Component {
     constructor(props) {
         super(props);
     }
+    componentDidMount() {
+        new Chart(document.getElementById(this.props.config.id).getContext('2d'), {
+            type: 'bar',
+            data: this.props.config.data,
+            options: {
+                title: {
+                    display: true,
+                    text: this.props.config.title
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false
+                },
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        stacked: true,
+                    }],
+                    yAxes: [{
+                        stacked: true
+                    }]
+                }
+            }
+        });
+    }
     render() {
         return (
             <div className="main-card mb-3 card">
                 <div className="card-body">
-                    <canvas id={this.props.chartId} height="100"></canvas>
+                    <canvas id={this.props.config.id} height="100"></canvas>
                 </div>
             </div>
         )
