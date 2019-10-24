3e78e4340eacaf07c105f3c7b12a7c47818e5217
Marcin Maciejewski <poczta.maciejewski@gmail.com>
2019-10-25 00:39:43 +0200

    Emails lists on tenants and apartment pages

--- src/TenantsComponent.js
+++ src/TenantsComponent.js
@@ -17,11 +17,17 @@ class TenantsComponent extends React.Component {
             title: false,
             clickableHash: "tenants",
             headers: ["Id", "Imię i Nazwisko", "Mieszkanie", "Pokój", "Czynsz"],
+            emails: data.map(x => {
+                return x.email;
+            }).filter((x) => {
+                return x != null;
+            }),
             rows: data.map(x => {
                 x.apartment_id = Utils.findArrValById(
                     apartments.map(a => [a.id, a.name]),
                     x.apartment_id
                 );
+                delete x.email;
                 return Object.values(x);
             })
         };
@@ -40,11 +46,17 @@ class TenantsComponent extends React.Component {
     }
     render() {
         return (
-            <div className="main-card mb-3 card">
-                {this.state.data ? <React.Fragment>
+            <React.Fragment>
+            {this.state.data ? <React.Fragment>
+                <div className="main-card mb-3 card">
                     <MTable tableData={this.state.data} />
-                </React.Fragment> : <LoaderComponent />}
-            </div>
+                </div>
+                <div className="card-shadow-info border mb-3 card card-body border-info">
+                    <h5 className="card-title">Emaile najemców</h5>
+                    {this.state.data.emails && this.state.data.emails.join(", ")}
+                </div>
+            </React.Fragment> : <LoaderComponent />}
+            </React.Fragment>
         )
     }
 }
